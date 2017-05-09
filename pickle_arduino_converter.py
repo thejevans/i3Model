f443e876dfe3da682d068db194eea08df6e563db
John Evans <thejevans@Johns-MacBook-Pro.local>
2017-05-09 17:41:11 -0400

    Completed rough converter minus dom/string conversion

--- pickle_arduino_converter.py
+++ pickle_arduino_converter.py
@@ -13,22 +13,106 @@ import cPickle
 import numpy as np
 
 ##################################################################################
+##### Converts from DOM/String format to LED value
+##################################################################################
+def flatten (dom, string):
+
+    return led
+
+##################################################################################
+##### Converts event to Array
+##################################################################################
+def eventToArray (event, maxBrightness, frames):
+    max_charge = max(event.T[3])
+    brightness = (event.T[3] * maxBrightness / max_charge).astype(int)
+
+    tbin       = np.arange(len(event.T[0])) / int(round(len(event.T[0]) / frames))
+    wavelength = (700. - 300. * tbin / frames).astype(int)
+
+    eventArray = np.array(None)
+
+    for i, pulse in enumerate(event):
+        [r, g, b] = wav2RGB(wavelength[i])
+
+        r  *= brightness[i]
+        g  *= brightness[i]
+        b  *= brightness[i]
+        led = flatten(pulse[1], pulse[2])
+
+        np.append(eventArray, [led, r, g, b])
+
+        if (i==len(event)-1) or (not wavelength[i]==wavelength[i+1]):
+            np.append(eventArray, ['n'])
+
+    return eventArray
+
+##################################################################################
+##### Converts from wavelength to RGB (0.0 to 1.0 scale)
+##################################################################################
+def wav2RGB(wavelength):
+    w = int(wavelength)
+
+    # color
+    if w >= 380 and w < 440:
+        R = -(w - 440.) / (440. - 350.)
+        G = 0.0
+        B = 1.0
+    elif w >= 440 and w < 490:
+        R = 0.0
+        G = (w - 440.) / (490. - 440.)
+        B = 1.0
+    elif w >= 490 and w < 510:
+        R = 0.0
+        G = 1.0
+        B = -(w - 510.) / (510. - 490.)
+    elif w >= 510 and w < 580:
+        R = (w - 510.) / (580. - 510.)
+        G = 1.0
+        B = 0.0
+    elif w >= 580 and w < 645:
+        R = 1.0
+        G = -(w - 645.) / (645. - 580.)
+        B = 0.0
+    elif w >= 645 and w <= 780:
+        R = 1.0
+        G = 0.0
+        B = 0.0
+    else:
+        R = 0.0
+        G = 0.0
+        B = 0.0
+
+    # intensity correction
+    if w >= 380 and w < 420:
+        SSS = 0.3 + 0.7*(w - 350) / (420 - 350)
+    elif w >= 420 and w <= 700:
+        SSS = 1.0
+    elif w > 700 and w <= 780:
+        SSS = 0.3 + 0.7*(780 - w) / (780 - 700)
+    else:
+        SSS = 0.0
+
+    return [int(SSS*R), int(SSS*G), int(SSS*B)]
+
+##################################################################################
 ##### Parsing variables
 ##################################################################################
-usage = "%prog [options] --infile <input pickled file> --outfile <output text file> --nevents <num of events>"
-parser = OptionParser(usage=usage)
-parser.add_option("-n", "--nevents", type="int", default = 0,
+usage  = "%prog [options] --infile <input pickled file> --outfile <output text file> --nevents <num of events>"
+parser = OptionParser(usage = usage)
+
+parser.add_option("-n", "--nevents", type = "int", default = 0,
                   help = "number of events to send to arduino/display")
 parser.add_option("-i", "--infile", type = "string",
-                  default = './event.p',
+                  default = './events.p',
                   help = "pickled file of all events")
 parser.add_option("-o", "--outfile", type = "string",
-                  default = './event.txt',
+                  default = './events.txt',
                   help = "text file of LED instructions")
+
 (options, args) = parser.parse_args()
-infile = options.infile
-outfile = options.outfile
-nevents = options.nevents
+infile          = options.infile
+outfile         = options.outfile
+nevents         = options.nevents
 
 print ('in_file: {0}'.format(infile))
 print ('out_file: {0}'.format(outfile))
@@ -38,41 +122,22 @@ print (' ')
 ##### Define basic variables
 ##################################################################################
 all_events = cPickle.load(open(infile, 'rb'))
-output = open(outfile, 'wb')
+output     = open(outfile, 'w')
+
 if nevents == 0: nevents = len(all_events)
+
 max_brightness = 100. ## set max brightness to avoid burning the LEDs
+frames = 32.
 
 ##################################################################################
-##### Send events to text file: thishit = [time, dom, string, charge]
+##### Send events to text file: hit = [time, dom, string, charge]
 ##################################################################################
 for i, event in enumerate(all_events):
-    if nth==nevents: break
+    if nth == nevents: break
 
-    ## CHARGE: get max PE for scaling brightness
-    max_charge = max(event.T[3])
-    brightness = (event.T[3]*max_brightness/max_charge).astype(int)
+    output.write(eventToArray(event, max_brightness, frames))
+    output.write("\nx\n")
 
-    ## TIME: array to identify which bin the hits are in (total 32 bins))
-    tbin = np.arange(len(event.T[0]))/ int(round(len(event.T[0])/32.))
-    wavelength = (700. - 300.*tbin / 32.).astype(int)
-
-    ## STRING: odd/even if two strings
-    strings = event.T[2]%2+1 ## remainder 0 = 1st string; remainder 1 = 2nd string
-    #strings = thisevent.T[2]
-
-    for j, pulse in enumerate(thisevent):
-        print ('### Pulse {0}: {1}, {2}, {3}, {4}'.format(j, wavelength[j], pulse[1], pulse[2], brightness[j]))
-
-        ## send data to arduino
-        data = str( (wavelength[j],pulse[1],strings[j],brightness[j]) )
-        send_data( data )
-
-        ## if this wavelength is different from next or if last pulse of this event;
-        ## tell arduino to display
-        if (j==len(event)-1) or (not wavelength[j]==wavelength[j+1]):
-            send_data( 'd' )
-            print ('### DISPLAY!')
-            print ('### ')
-
-    print ('### ')
     nth += 1
+
+output.close()
