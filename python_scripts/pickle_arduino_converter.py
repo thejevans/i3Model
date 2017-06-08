8d886613c7d16fcd246887ae50f8abd974911ec0
John Evans <thejevans@Johns-MacBook-Pro.local>
2017-06-08 17:09:37 -0400

    fixed line 157

--- python_scripts/pickle_arduino_converter.py
+++ python_scripts/pickle_arduino_converter.py
@@ -154,9 +154,9 @@ nth = 0
 for i, event in enumerate(all_events):
     if nth == nevents: break
 
-    print (event['pid'])
+    print (event['id'])
 
-    output = open(dir_path + event['pid'] + '.I3R', 'w')
+    output = open(dir_path + event['id'] + '.I3R', 'w')
 
     output.write("q\n%s\n%s\n%s\n%s\n%s\n" % (event['date'], event['id'], event['energy'], event['zenith'], event['pid']))
 
